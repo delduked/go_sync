f8a7bf5eefa5376def7a1daf90e224078d1a9f47
nate.delduca@cantire.com <nate.delduca@cantire.com>
2024-09-13 10:49:08 -0400

    Save service not creating file when file doesn't exist

--- internal/services/save.service.go
+++ internal/services/save.service.go
@@ -13,6 +13,17 @@ func Save(req *pb.FileChunk, stream grpc.BidiStreamingServer[pb.FileSyncRequest,
 	filePath := filepath.Join("./sync_folder", req.FileName)
 	log.Printf("Saving file chunk: %s", filePath)
 
+	// check if file exists
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// Create the file if it doesn't exist
+		file, err := os.Create(filePath)
+		if err != nil {
+			log.Errorf("Error creating file: %v", err)
+			return
+		}
+		file.Close()
+	}
+
 	// Open the file (or create if it doesn't exist), and append the chunk
 	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
