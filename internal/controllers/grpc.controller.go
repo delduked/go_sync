a57e49b7f1054c1dd022b3e0dafc414902096fac
nate.delduca@cantire.com <nate.delduca@cantire.com>
2024-09-14 23:48:24 -0400

    editing chunks

--- internal/controllers/grpc.controller.go
+++ internal/controllers/grpc.controller.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	pb "go_sync/filesync"
 	"go_sync/internal/services"
-	"go_sync/pkg"
 	"io"
 	"os"
 	"path/filepath"
@@ -46,33 +45,31 @@ func (s *FileSyncServer) SyncFiles(stream pb.FileSyncService_SyncFilesServer) er
 
 func (s *FileSyncServer) Save(req *pb.FileChunk, stream grpc.BidiStreamingServer[pb.FileSyncRequest, pb.FileSyncResponse]) {
 	filePath := filepath.Clean(req.FileName)
-	log.Printf("Saving file chunk: %s", filePath)
-
-	// Add the file to SyncedFiles
-	s.SharedData.mu.Lock()
-	if pkg.ContainsString(s.SharedData.SyncedFiles, filePath) {
-		log.Printf("File %s is already being synchronized. Ignoring.", filePath)
-		s.SharedData.mu.Unlock()
-		return
+	isFirstChunk := req.ChunkNumber == 1
+	var file *os.File
+	if isFirstChunk {
+		// Truncate the file if it's the first chunk
+		file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Errorf("Error opening file: %v", err)
+			s.SharedData.markFileAsComplete(filePath)
+		}
+		defer file.Close()
+	} else {
+		// Open the file in append mode for subsequent chunks
+		file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Errorf("Error opening file: %v", err)
+			s.SharedData.markFileAsComplete(filePath)
+		}
+		defer file.Close()
 	}
-	s.SharedData.SyncedFiles = append(s.SharedData.SyncedFiles, filePath)
-	s.SharedData.mu.Unlock()
-
-	// Open the file in append mode
-	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Errorf("Error opening file: %v", err)
-		s.SharedData.markFileAsComplete(filePath)
-		return
-	}
-	defer file.Close()
 
 	// Write the chunk to the file
-	_, err = file.Write(req.ChunkData)
+	_, err := file.Write(req.ChunkData)
 	if err != nil {
-		log.Errorf("Error writing file: %v", err)
+		log.Errorf("Error writing chunk %d of file %s: %v", req.ChunkNumber, req.FileName, err)
 		s.SharedData.markFileAsComplete(filePath)
-		return
 	}
 
 	// Check if the transfer is complete
